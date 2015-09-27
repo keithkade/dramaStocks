9c25e498828385bfac89e288ce8eb5c957b7c0b2
Bear Cott <bear@bearcott.com>
2015-09-27 07:27:59 -0500

    polarity and subjectivity

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app, mongo
+from textblob import TextBlob
 
 import nltk
 
 @app.route('/')
 def home():
     #prices = list(mongo.db.prices.find({},{'ticker':1}))
-    prices = list(mongo.db.tweets.find({'name':'IBM'}))
+    prices = list(mongo.db.tweets.find({}))
 
     return render_template('index.html', prices=prices)
