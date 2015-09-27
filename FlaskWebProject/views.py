40ee13b752390ef739026927248422e4d417d68f
keithkade <keithkade@aol.com>
2015-09-27 07:30:26 -0500

    rename mongoupdate to trader

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app, mongo
+from trader import Trader
 
 import nltk
 
@@ -8,5 +9,8 @@ import nltk
 def home():
     #prices = list(mongo.db.prices.find({},{'ticker':1}))
     prices = list(mongo.db.tweets.find({'name':'IBM'}))
+    for thing in  prices[0]['tweets']:
+        print thing['retweet']
+        print thing['date']
 
     return render_template('index.html', prices=prices)
