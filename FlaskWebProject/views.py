5f3a1ed68074798d4682de53126aebe48bcd15a9
keithkade <keithkade@aol.com>
2015-09-27 08:38:25 -0500

    added bad logic for buying and selling

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app, mongo
-#from trader import Trader
+from trader import Trader
 from textblob import TextBlob
 from dateutil.parser import parse
 
@@ -13,21 +13,27 @@ def home():
     prices = list(mongo.db.tweets.find({'name':'IBM'}))
 
 
-    baught = False
+    #begin stupid stop picking
+
+    bought = False
+    highRetweet = 0
     buyDate = ''
     sellDate = ''
     for thing in  prices[0]['tweets']:
         reCount =  thing['retweet']
         dateStr = parse(thing['date']).strftime('%Y-%m-%d')
-        if reCount > 0:
-            baught = True
+        if highRetweet < reCount:
+            highRetweet = reCount
+
+        if reCount > 0 and not bought and buyDate == '':
+            bought = True
             buyDate = dateStr
-        if reCount == 0 and baught:
-            baught = False
+
+        if reCount < highRetweet and bought and dateStr > buyDate and sellDate == '':
+            bought = False
             sellDate = dateStr
 
-    print buyDate
-    print sellDate
     print Trader.buySell('IBM', buyDate, sellDate, 1000)
+    print Trader.buySell('DJI', buyDate, sellDate, 1000)
 
     return render_template('index.html', prices=prices)
